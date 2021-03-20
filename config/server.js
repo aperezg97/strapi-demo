4f75f0848db5f40dd0bfbab6795a551512ce53a5
Alex Perez <alex27_perez@hotmail.com>
2021-03-20 15:47:15 -0600

    Renaming .env variables

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('APP_HOST', '0.0.0.0'),
-  port: env.int('APP_PORT', 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '2c631d3ed3802341b2e4608072033521'),
